601dc8da6553d15f6d3af69822b6cdac33fcbeb5
MatthewUbalde <matthew.ubalde@gmail.com>
2024-05-10 13:09:32 -0400

    Filled in the Game page
    And this includes filling in some of the data

--- src/data/game.ts
+++ src/data/game.ts
@@ -6,6 +6,14 @@ const gameProjects: Array<ProjectShowcase> = [
     src: "/mint-big.png",
     href: "/",
     alt: "Dark Birthday eating cheese in his dreams",
+    description: "King of the Hat fan-game where you play as Dark Birthday and eat cheese in his dreams!",
+  },
+  {
+    title: "Combo Spells",
+    src: "/mint-big.png",
+    href: "/",
+    alt: "Magician beating up a dummy",
+    description: "Initially a high school project. Use series of key combinations to cast spells upon the dummy and get a high score!",
   }
 ]
 
