34cce9754b6a3ff7636ac884cfc68148025e3f31
MatthewUbalde <matthew.ubalde@gmail.com>
2024-05-10 11:13:33 -0400

    Readjust the image filepath to public

--- src/data/softwareExperience.ts
+++ src/data/softwareExperience.ts
@@ -4,43 +4,43 @@ const softwareExperience: Array<ExperienceShowcase> = [
   {
     title: "C++",
     experience: "Intermediate",
-    src: "../assets/c++.svg",
+    src: "/assets/c++.svg",
     alt: "Purple logo of C++"
   },
   {
     title: "Java",
     experience: "Intermediate",
-    src: "../assets/java.svg",
+    src: "/assets/java.svg",
     alt: "A cup of coffee logo of Java"
   },
   {
     title: "C#",
     experience: "Intermediate",
-    src: "../assets/c-sharp.svg",
+    src: "/assets/c-sharp.svg",
     alt: "Blue logo of C#"
   },
   {
     title: "MySQL",
     experience: "Beginner",
-    src: "../assets/mysql.svg",
+    src: "/assets/mysql.svg",
     alt: "MySql Logo"
   },
   {
     title: "OpenGL",
     experience: "Beginner",
-    src: "../assets/c++.svg",
+    src: "/assets/c++.svg",
     alt: "OpenGL Logo"
   },
   {
     title: "Android Studio",
     experience: "Beginner",
-    src: "../assets/android-studio-stable.svg",
+    src: "/assets/android-studio-stable.svg",
     alt: "Android Studio Logo"
   },
   {
     title: "React Native",
     experience: "Beginner",
-    src: "../assets/react.svg",
+    src: "/assets/react.svg",
     alt: "React Native Logo"
   }
 ]
