daf4e09ce71cf0d0140f7b72d7e7db53ac935eda
MatthewUbalde <matthew.ubalde@gmail.com>
2024-05-14 13:15:41 -0400

    Added links to each Game Project
    To be navigated in the pages

--- src/data/game.ts
+++ src/data/game.ts
@@ -4,14 +4,14 @@ const gameProjects: Array<ProjectShowcase> = [
   {
     title: "King of the Cheese",
     src: "/mint-big.png",
-    href: "/",
+    href: "/game/king-of-the-cheese",
     alt: "Dark Birthday eating cheese in his dreams",
     description: "King of the Hat fan-game where you play as Dark Birthday and eat cheese in his dreams!",
   },
   {
     title: "Combo Spells",
     src: "/mint-big.png",
-    href: "/",
+    href: "/game/combo-spells",
     alt: "Magician beating up a dummy",
     description: "Initially a high school project. Use series of key combinations to cast spells upon the dummy and get a high score!",
   }
