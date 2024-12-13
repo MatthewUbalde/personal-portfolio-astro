37879a22a038cb24a056223ee17da6ce3dbe860e
Matthew Ubalde <matthew.ubalde@gmail.com>
2024-12-13 11:50:34 -0500

    remove vue from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import vue from "@astrojs/vue";
 
 import tailwind from "@astrojs/tailwind";
 import { twJoin, twMerge } from "tailwind-merge";
@@ -9,7 +8,6 @@ export default defineConfig({
   site: "https://matthewubalde.github.io",
   base: "",
   integrations: [
-    vue(),
     tailwind({
       applyBaseStyles: true,
     }),
