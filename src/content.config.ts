7f52effa0606e75a4b1671d31e338a3a074cbae3
Matthew Ubalde <matthew.ubalde@gmail.com>
2024-12-13 16:20:43 -0500

    figure out how content collections works. bun doesn't work

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,2 +1,30 @@
 import { defineCollection, z } from "astro:content";
 import { file } from "astro/loaders";
+
+import { experienceBadgeSchema, projectCoverSchema } from "./schemas";
+
+const gameProjects = defineCollection({
+  loader: file("src/data/game.json", {
+    parser: (text) => JSON.parse(text).projects,
+  }),
+  schema: projectCoverSchema,
+});
+
+const gameExperiences = defineCollection({
+  loader: file("src/data/game.json", {
+    parser: (text) => JSON.parse(text).experience,
+  }),
+  schema: experienceBadgeSchema,
+});
+
+const game = defineCollection({
+  loader: file("src/data/game.json", {
+    parser: (text) => JSON.parse(text),
+  }),
+  schema: z.object({
+    projects: projectCoverSchema,
+    experience: experienceBadgeSchema,
+  }),
+});
+
+export const collections = { game, gameProjects, gameExperiences };
